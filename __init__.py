8fb97de7784cfe965cab746945295e447b54bc86
Shikhar Chauhan <shikhar.chauhan@live.com>
2017-04-04 13:26:20 +0530

    Fixing default args

--- __init__.py
+++ __init__.py
@@ -7,8 +7,9 @@ def assemble(func):
 	def wrapper(self , *args , **kargs):
 		for name,arg in itertools.chain(zip(names[1:] , args) , kargs.items()):
 			setattr(self,name , arg)
-		for name,default in zip(reversed(names),reversed(defaults)):
-			if not hasattr(self,name):
-				setattr(self , name , default)
+		if defaults:
+			for name,default in zip(reversed(names),reversed(defaults)):
+				if not hasattr(self,name):
+					setattr(self , name , default)
 		func(self , *args , **kargs)
 	return wrapper
